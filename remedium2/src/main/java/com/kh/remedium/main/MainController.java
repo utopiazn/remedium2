ea404ef74055a14793675a94814bf1255f2845ef
KH_I\user1 <utopiazn@naver.com>
2017-06-05 15:16:21 +0900

    dfdfdf

---
+++ remedium2/src/main/java/com/kh/remedium/main/MainController.java
@@ -0,0 +1,16 @@
+package com.kh.remedium.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	
+	@RequestMapping("/main")
+	public String main(){
+		
+		return "main4";
+	}
+	
+}
---
+++ remedium2/src/main/webapp/WEB-INF/main/main3.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="UTF-8"> 
+<title></title>
+</head>
+
+<body>
+		
+		
+		rdstrstettst
+	
+		
+</body>
+
+
+</html>
