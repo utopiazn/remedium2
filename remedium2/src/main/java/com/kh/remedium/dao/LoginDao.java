469685238a721149df00a19d4a5403d7133b2831
허어녕 <hueonyung>
2017-06-07 14:09:10 +0900

    no message

--- remedium2/src/main/java/com/kh/remedium/dao/LoginDao.java
+++ remedium2/src/main/java/com/kh/remedium/dao/LoginDao.java
@@ -3,7 +3,11 @@ package com.kh.remedium.dao;
 import com.kh.remedium.model.MemberModel;
 
 public interface LoginDao {
+	
 	public MemberModel memberLogin(MemberModel member);
 	
+	public Object memberModify(MemberModel member);
+	
+	public Object memberDelete(MemberModel member);
 	
 }
--- remedium2/src/main/java/com/kh/remedium/service/LoginService.java
+++ remedium2/src/main/java/com/kh/remedium/service/LoginService.java
@@ -1,5 +1,34 @@
 package com.kh.remedium.service;
 
-public class LoginService {
+import javax.annotation.Resource;
 
+import com.kh.remedium.dao.LoginDao;
+import com.kh.remedium.model.MemberModel;
+import org.mybatis.spring.SqlSessionTemplate;
+
+public class LoginService implements LoginDao{
+	
+	@Resource
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	@Override
+	public MemberModel memberLogin(MemberModel member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object memberModify(MemberModel member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object memberDelete(MemberModel member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 }
