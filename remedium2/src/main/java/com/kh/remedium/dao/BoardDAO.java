b7972b277e46b23027778aec1bc9e6c2811d94c2
허어녕 <hueonyung>
2017-06-14 20:35:21 +0900

    문의 게시판 리스트 , 글 보기

---
+++ remedium2/src/main/java/com/kh/remedium/dao/BoardDAO.java
@@ -0,0 +1,38 @@
+package com.kh.remedium.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardDAO extends AbstractDAO {
+
+	public void insert(Map<String,Object> map) {
+		insert("board.insertBoard", map);
+	}
+
+	public void update(Map<String,Object> map) {
+		update("board.updateBoard", map);
+	}
+
+	public void delete(Map<String,Object> map) {
+		delete("board.deleteBoard", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> selectOne(Map<String,Object> map) {
+		// TODO Auto-generated method stub
+		return (Map<String, Object>) selectOne("board.selectOne", map);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Map<String,Object>> selectList(Map<String,Object> map) {
+		// TODO Auto-generated method stub
+		return selectList("board.selectAll");
+	}
+
+
+
+	
+}
---
+++ remedium2/src/main/java/com/kh/remedium/service/BoardServiceImpl.java
@@ -0,0 +1,48 @@
+package com.kh.remedium.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.kh.remedium.dao.BoardDAO;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+	
+	@Resource
+	private BoardDAO boardDAO;
+	
+	
+	public List<Map<String, Object>> selectAll(Map<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub
+		return boardDAO.selectList(map);
+	}
+
+	@Override
+	public Map<String, Object> selectOne(Map<String, Object> map) throws Exception {
+	
+		return boardDAO.selectOne(map);
+	}
+
+	@Override
+	public void insertBoard(Map<String, Object> map) throws Exception {
+		boardDAO.insert(map);
+		
+	}
+
+	@Override
+	public void updateBoard(Map<String, Object> map) throws Exception {
+		boardDAO.update(map);
+		
+	}
+
+	@Override
+	public void deleteBoard(Map<String, Object> map) throws Exception {
+		boardDAO.delete(map);
+		
+	}
+	
+}
