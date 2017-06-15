0990b2a97e510b7254ad04591db13e65db331d7f
anyeah <imyeeah@naver.com>
2017-06-15 09:14:56 +0900

    뀨

--- remedium2/src/main/java/com/kh/remedium/mypage/CashController.java
+++ remedium2/src/main/java/com/kh/remedium/mypage/CashController.java
@@ -1,12 +1,21 @@
 package com.kh.remedium.mypage;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
+import org.aspectj.org.eclipse.jdt.core.dom.ThisExpression;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import com.kh.remedium.common.CommandMap;
+
+/*@Controller
 public class CashController {
 	
 	ModelAndView mav = new ModelAndView();
@@ -18,6 +27,25 @@ public class CashController {
 		mav.setViewName("pageListCash");
 		
 		return mav;
-	}
+	}*/
 
+@Controller()
+@RequestMapping(value="myPageCashForm")
+public class CashController{
+	
+	Logger logger = Logger.getLogger(this.getClass());
+	
+	/*@Resource(name="cashService")
+	private CashService cashService;*/
+
+	
+	@RequestMapping(value="/?")
+	public String myPageCashForm (CommandMap commandMap, Model model) throws Exception{
+		
+		/*List<Map<String, Object>> list = cashService.*/
+		
+		/*model.addAllAttributes("list", list);*/
+		
+		return "main3";
+	}
 }
